e8d0b6b92c188660d886e9d14183dfec43c37a66
Bharath <bharath0996@gmail.com>
2021-12-07 10:40:04 +0530

    Added AuthenticationActivity, Designed some Main activity

--- app/src/main/java/com/user/goservice/MainActivity.java
+++ app/src/main/java/com/user/goservice/MainActivity.java
@@ -1,8 +1,9 @@
 package com.user.goservice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, AuthenticationActivity.class);
+        startActivity(intent);
     }
 }
