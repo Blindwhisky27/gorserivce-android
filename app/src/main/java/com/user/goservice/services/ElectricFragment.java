42eb47bd91c065f3fe4b1ba3dca191c681d99f6e
Bharath <bharath0996@gmail.com>
2021-12-12 21:58:21 +0530

    Added fragments for services

---
+++ app/src/main/java/com/user/goservice/services/ElectricFragment.java
@@ -0,0 +1,30 @@
+package com.user.goservice.services;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.user.goservice.R;
+
+
+
+public class ElectricFragment extends Fragment {
+
+
+
+    public ElectricFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_electric, container, false);
+    }
+}
