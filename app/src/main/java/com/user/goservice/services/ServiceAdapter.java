ef56c92bea22a655580b3f0d24b0b6f0c263f1f6
Bharath <bharath0996@gmail.com>
2021-12-15 15:00:50 +0530

    Working on Service Adapter

---
+++ app/src/main/java/com/user/goservice/services/ServiceAdapter.java
@@ -0,0 +1,60 @@
+package com.user.goservice.services;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.user.goservice.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class ServiceAdapter extends RecyclerView.Adapter<ServiceAdapter.ViewHolder> {
+    private ArrayList<Service> service;
+    private Context context;
+
+    public ServiceAdapter(ArrayList<Service> service, Context context) {
+        this.service = service;
+        this.context = context;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ServiceAdapter.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.serivce_items, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ServiceAdapter.ViewHolder holder, int position) {
+        holder.serviceName.setText(service.get(position).serviceName);
+        holder.price.setText(service.get(position).price);
+    }
+
+    @Override
+    public int getItemCount() {
+        return service.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView serviceName, price;
+        private android.widget.Button add, remove;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            serviceName = itemView.findViewById(R.id.serviceName);
+            price = itemView.findViewById(R.id.priceTextView);
+            add = itemView.findViewById(R.id.addItemButton);
+            remove = itemView.findViewById(R.id.removeItemButton);
+
+        }
+    }
+}
